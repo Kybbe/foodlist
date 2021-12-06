bc0f429155414237876b81bf7c3c24b03be6cc74
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2021-12-06 11:37:56 +0100

    move around and some other stuff

--- src/components/recipe.vue
+++ src/components/recipe.vue
@@ -10,15 +10,15 @@
         padding: 0px;
       "
     >
-      <mainArea :title="title" :description="description" :imgLink="imgLink" />
+      <mainArea :title="title" :description="description" :imgLink="recipe.imgLink" />
     </div>
 
     <div class="mediumCard card">
-      <ingredients :ingredients="ingredients" />
+      <ingredients :ingredients="recipe.ingredients" />
     </div>
 
     <div class="mediumCard card">
-      <instruction :instructions="instructions" />
+      <instruction :instructions="recipe.instructions" />
     </div>
   </div>
 </template>
@@ -30,53 +30,19 @@ import ingredients from "./ingredients.vue";
 import Navbar from "./navbar.vue";
 
 export default {
-  name: "App",
+  name: "recipe",
   components: {
     mainArea,
     instruction,
     ingredients,
     Navbar,
   },
-  data: function () {
-    return {
-      recipeId: 1,
-      title: "Pasta med ost- och skinksås",
-      description:
-        "Ibland vill man bara fixa ihop en enkel men god pasta till middag! Då har du denna krämiga pastarätt med ost- och skinksås. Prästost ger en perfekt smak till såsen, och du adderar gräslök och tomater på toppen innan servering!",
-      instructions: [
-        {
-          id: 0,
-          checked: false,
-          text: "Koka pastan enligt anvisningen på förpackningen.",
-        },
-        {
-          id: 1,
-          checked: false,
-          text: "Blanda i grädden och majsstärkelsen utrörd i mjölken. Koka upp under upprörning. Rör i skinkan och osten och låt allt bli varmt. Smaka av med salt och peppar.",
-        },
-        { id: 2, checked: false, text: "Skär tomaterna och hacka gräslöken." },
-        {
-          id: 3,
-          checked: false,
-          text: "Servera pastan med ost- och skinksåsen, tomater och gräslök.",
-        },
-      ],
-      ingredients: [
-        { amount: 4, measurment: "Portioner", name: "Pasta" },
-        { amount: 1, measurment: "msk", name: "Smör eller olja" },
-        { amount: 2.5, measurment: "dl", name: "Matlagningsgrädde" },
-        { amount: 1.5, measurment: "dl", name: "Mjölk" },
-        { amount: 2, measurment: "tsk", name: "Majsstärkelse" },
-        { amount: 2, measurment: "Förpackningar", name: "Strimlad Skinka" },
-        { amount: 1.5, measurment: "dl", name: "Riven smakrik ost" },
-        { amount: "", measurment: "", name: "Salt" },
-        { amount: "", measurment: "", name: "Peppar" },
-      ],
-      link: "https://www.ica.se/recept/pasta-med-kramig-ost-och-skinksas-725815/",
-      imgLink:
-        "https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/imagevaultfiles/id_207924/cf_259/pasta_med_kramig_ost-_och_skinksas.jpg",
-    };
-  },
+  props: {
+    recipe: {
+      type: Object,
+      required: true,
+    },
+  }
 };
 </script>
 
--- src/views/recipe-list.vue
+++ src/views/recipe-list.vue
@@ -1,95 +1,96 @@
 <template>
-	<div style="text-align: center; background-color: lightblue;">
-		<h1>Recipes:</h1>
-	</div>
-	<div class="cardList">
-		<div class="long-card" v-for="partOfList in recipesList" :key="partOfList.title">
-			<div class="backgroundIMG"></div>
-			<div class="textPart">
-				<h2>{{ partOfList.title }}</h2>
-				<p>{{ partOfList.description }}</p>
-			</div>
-		</div>
-	</div>
+  <div style="text-align: center; background-color: lightblue">
+    <h1>Recipes:</h1>
+  </div>
+  <div class="cardList">
+    <div
+      class="long-card"
+      v-for="partOfList in recipesList"
+      :key="partOfList.title"
+			v-on:click="test(partOfList.recipeId)"
+    >
+      <div class="backgroundIMG"></div>
+      <div class="textPart">
+        <h2>{{ partOfList.title }}</h2>
+        <p>{{ partOfList.description }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-			recipesList: [
-				{
-				recipeId: 1,
-				title: "Pasta med ost- och skinksås",
-				description:
-					"Ibland vill man bara fixa ihop en enkel men god pasta till middag! Då har du denna krämiga pastarätt med ost- och skinksås. Prästost ger en perfekt smak till såsen, och du adderar gräslök och tomater på toppen innan servering!",
-				},
-				{
-				recipeId: 2,
-				title: "Pasta med gräddfil",
-				description:
-					"Ibland vill"
-				},
-			]
-    }
-  }
-}
+  name: "RecipeList",
+  props: {
+    recipesList: {
+      type: Array,
+      required: true,
+    },
+  },
+	methods: {
+		test: function(id) {
+			console.log(id);
+		}
+	}
+};
 </script>
 
 <style lang="scss" scoped>
 h1 {
-	margin-top: 0px;
+  margin-top: 0px;
 }
 
 .cardList {
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-evenly;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 }
 
 .long-card {
-	width: calc(100% - 20px);
-	max-width: 400px;
-	border: 1px solid black;
-	background-color: white;
-	border-radius: 10px;
-	position: relative;
-	margin-bottom: 10px;
-	cursor: pointer;
+  width: calc(100% - 20px);
+  max-width: 400px;
+  border: 1px solid black;
+  background-color: white;
+  border-radius: 10px;
+  position: relative;
+  margin-bottom: 10px;
+  cursor: pointer;
 }
 
 .backgroundIMG {
-	background-image: url("https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/imagevaultfiles/id_207924/cf_259/pasta_med_kramig_ost-_och_skinksas.jpg");
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-position: center center;
-	opacity: 0.6;
-	height: 100%;
-	width: 100%;
-	position: absolute;
-	border-radius: inherit;
-	filter: saturate(70%);
+  background-image: url("https://assets.icanet.se/e_sharpen:80,q_auto,dpr_1.25,w_718,h_718,c_lfill/imagevaultfiles/id_207924/cf_259/pasta_med_kramig_ost-_och_skinksas.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  opacity: 0.6;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  border-radius: inherit;
+  filter: saturate(70%);
 }
 
 .textPart {
-	padding: 10px;
-	border-radius: inherit;
-	backdrop-filter: blur(1px);
-	background-color: rgba(255, 255, 255, 0.4);
+  padding: 10px;
+  border-radius: inherit;
+  backdrop-filter: blur(1px);
+  background-color: rgba(255, 255, 255, 0.4);
+  width: calc(100% - 20px);
+  height: calc(100% - 20px);
 
-	h2 {
-		text-align: center;
-		margin: 0;
-		margin-bottom: 4px;
-	}
+  h2 {
+    text-align: center;
+    margin: 0;
+    margin-bottom: 4px;
+  }
 
-	p {
-		//max-height: calc(3.6em + 32px);
-		display: -webkit-box;
-		-webkit-line-clamp: 4;
-		-webkit-box-orient: vertical;
-		overflow: hidden;
-		margin-bottom: 0px;
-	}
+  p {
+    //max-height: calc(3.6em + 32px);
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    margin-bottom: 0px;
+  }
 }
 </style>
