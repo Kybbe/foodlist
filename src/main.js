8901dcca76c41392f1717cc10513ee0f235b9e03
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2022-03-27 20:51:52 +0200

    idk fix the stuff i poopoo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import firebase from "firebase/app";
-import "firebase/auth";
 
 import store from "./store/index"; // short for @/store/index.js
 
