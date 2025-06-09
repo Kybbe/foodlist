5f679b0be9877e0ab4a258c757626e9343219dc2
Jacob Klaren <jacob.klaren@outlook.com>
2025-06-09 20:15:07 +0200

    add new meatball recipe image

--- src/components/images.js
+++ src/components/images.js
@@ -2,12 +2,14 @@ import kramigProsciuttopasta from "../assets/kramigProsciuttopasta.jpg";
 import asiatiskFlaskwok from "../assets/asiatiskFlaskwok.avif";
 import pannstektMedelhavskyckling from "../assets/pannstektMedelhavskyckling.avif";
 import sweetChiliFlaskkott from "../assets/sweetChiliFlaskkott.avif";
+import kottbullarPotatismosLok from "../assets/KottbullarPotatismosLök.avif";
 
 const images = {
   kramigProsciuttopasta,
   asiatiskFlaskwok,
   pannstektMedelhavskyckling,
   sweetChiliFlaskkott,
+  kottbullarPotatismosLok,
 };
 
 export default function Images() {
