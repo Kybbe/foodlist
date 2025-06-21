4ab21bf394302bc7184a4e5ce771510e2fe9d72d
Jacob Klaren <jacob.klaren@outlook.com>
2025-06-21 09:58:10 +0200

    new edit recipe using primevue components

--- src/views/edit-recipe.vue
+++ src/views/edit-recipe.vue
@@ -1,60 +1,86 @@
 <template>
   <div>
     <form action="" id="container">
-      <input type="text" name="title" id="title" placeholder="title" />
-      <label for="drink">drink? :</label>
-      <input type="checkbox" name="drink" id="drink" />
-      <input
+      <InputText type="text" name="title" id="title" placeholder="title" v-model="recipe.title" />
+      <label for="drink">drink?</label>
+      <Checkbox 
+        id="drink"
+        name="drink"
+        :value="recipe.drink"
+        v-model="recipe.drink"
+      />
+      <InputText
         type="text"
         name="description"
         id="description"
         placeholder="description"
+        v-model="recipe.description"
       />
-      <input type="text" name="imgLink" id="imgLink" placeholder="imgLink" />
+      <InputText
+        type="text"
+        name="imgLink"
+        id="imgLink"
+        placeholder="imgLink"
+        v-model="recipe.imgLink"
+      />
+
       <div id="labels">
         <label for="amount" style="flex: 0.25">Quantity:</label>
         <label for="ingredientUnit" style="flex: 0.25">Unit:</label>
         <label for="ingredientName" style="flex: 0.65">Ingredient Name:</label>
         <label for="ingredientSection" style="flex: 0.25">Section:</label>
       </div>
-      <div id="ingredientsList"></div>
-      <div id="ingredientsButtons">
-        <button type="button" v-on:click="addIngredient">Add Ingredient</button>
-        <button
-          type="button"
-          v-on:click="removeIngredient"
-          v-if="moreThanOneIngredient"
-        >
-          Remove Ingredient
-        </button>
+      <div id="ingredientsList">
+        <div v-for="(ingredient, index) in recipe.ingredients" :key="index" class="editIngredients">
+          <InputText type="text" v-model="ingredient.amount" placeholder="Amount" style="flex: 0.25" />
+          <InputText type="text" v-model="ingredient.measurment" placeholder="Unit" style="flex: 0.25" />
+          <AutoCompletingIngredientInput v-model="ingredient.name" placeholder="Ingredient Name" style="flex: 0.65" />
+          <InputText type="text" v-model="ingredient.section" placeholder="Section" style="flex: 0.25" />
+					<Button variant="text" type="button" @click="removeIngredientAt(index)" class="removeIngredientBtn" style="flex: 0.1">-</Button>
+					<Button
+						variant="text"
+						v-if="index === recipe.ingredients.length - 1"
+						type="button"
+						@click="addIngredient"
+						class="addIngredientBtn"
+						style="flex: 0.1"
+					>
+						+
+					</Button>
+        </div>
       </div>
+      
       <div id="labels">
-        <label for="checked" style="flex: 0.25">Checked:</label>
-        <label for="id" style="flex: 0.25">Id:</label>
+        <label for="checked" style="flex: 0.15">Checked:</label>
+        <label for="id" style="flex: 0.15">Id:</label>
         <label for="name" style="flex: 0.8">Instruction:</label>
       </div>
-      <div id="instructionsList"></div>
-      <div id="instructionsButtons">
-        <button type="button" v-on:click="addInstruction">
-          Add Instruction
-        </button>
-        <button
-          type="button"
-          v-on:click="removeInstruction"
-          v-if="moreThanOneInstruction"
-        >
-          Remove Instruction
-        </button>
+      <div id="instructionsList">
+        <div v-for="(instruction, index) in recipe.instructions" :key="index" class="editInstructions">
+          <Checkbox
+            v-model="instruction.checked"
+            :id="'instruction-' + index"
+            style="flex: 0.15"
+          />
+          <InputText type="number" v-model="instruction.id" placeholder="Id" style="flex: 0.15" />
+          <InputText type="text" v-model="instruction.text" placeholder="Instruction" style="flex: 0.8" />
+          <Button variant="text" type="button" @click="removeInstruction" class="removeInstructionBtn" style="flex: 0.1">-</Button>
+          <Button
+            variant="text"
+            v-if="index === recipe.instructions.length - 1"
+            type="button"
+            @click="addInstruction"
+            class="addInstructionBtn"
+            style="flex: 0.1"
+          >
+            +
+          </Button>
+        </div>
       </div>
-      <input type="text" name="servings" id="servings" placeholder="servings" />
-      <input type="text" name="link" id="link" placeholder="link" />
-      <button
-        v-on:click.prevent="editFirebase"
-        type="submit"
-        id="updateRecipeBtn"
-      >
-        Update
-      </button>
+
+      <InputText type="text" name="servings" id="servings" placeholder="servings" v-model="recipe.servings" />
+      <InputText type="text" name="link" id="link" placeholder="link" v-model="recipe.link" />
+      <Button @click.prevent="editFirebase" type="submit" id="updateRecipeBtn">Update</Button>
     </form>
   </div>
 </template>
@@ -63,199 +89,89 @@
 import firebase from "firebase/app";
 import "firebase/database";
 import AutoCompletingIngredientInput from "../components/autoCompletingIngredientInput.vue";
+import { Button, Checkbox, InputText } from "primevue";
 
 export default {
-	name: "edit-recipe",
-	components: {
-		AutoCompletingIngredientInput,
-	},
-	data() {
-		return {
-			moreThanOneIngredient: false,
-			moreThanOneInstruction: false,
-		};
-	},
-	methods: {
-		addIngredient() {
-			const ingredientTemplate = document
-				.getElementsByClassName("editIngredients")[0]
-				.cloneNode(true);
-
-			for (let i = 0; i < ingredientTemplate.length; i++) {
-				ingredientTemplate.querySelector("input").value = "";
-			}
-
-			// set value of all inputs in ingredientTemplate to empty string
-			document
-				.getElementsByClassName("editIngredients")[0]
-				.parentNode.appendChild(ingredientTemplate);
-
-			this.moreThanOneIngredient = true;
+  name: "edit-recipe",
+  components: {
+    AutoCompletingIngredientInput,
+    Button,
+    InputText,
+    Checkbox,
+  },
+  data() {
+    return {
+      recipe: {
+        title: "",
+        drink: false,
+        description: "",
+        imgLink: "",
+        ingredients: [
+          { amount: "", measurment: "", name: "", section: "" },
+        ],
+        instructions: [
+          { checked: false, id: 0, text: "" },
+        ],
+        servings: "",
+        link: "",
+      },
+    };
+  },
+  methods: {
+    addIngredient() {
+      this.recipe.ingredients.push({ amount: "", measurment: "", name: "", section: "" });
+    },
+    removeIngredient() {
+      if (this.recipe.ingredients.length > 1) {
+        this.recipe.ingredients.pop();
+      }
 		},
-		removeIngredient() {
-			const ingredients = document.getElementsByClassName("editIngredients");
-			if (ingredients.length > 1) {
-				ingredients[ingredients.length - 1].remove();
-			}
-			if (ingredients.length === 1) {
-				this.moreThanOneIngredient = false;
-			}
-		},
-		addInstruction() {
-			const list = document.getElementById("instructionsList");
-
-			const newInstruction = list.children[list.children.length - 1]; // get the bar where we input text (bottom one)
-
-			list.insertBefore(newInstruction.cloneNode(true), newInstruction); // add the new ingredient to the top of the list
-
-			const instructionInputs = newInstruction.getElementsByTagName("input");
-			for (let i = 0; i < instructionInputs.length; i++) {
-				//for all inputs in ingredientInputs, set value as ""
-				instructionInputs[i].value = "";
-			}
-
-			this.moreThanOneInstruction = true;
-		},
-		removeInstruction() {
-			const instructions = document.getElementsByClassName("editInstructions");
-			if (instructions.length > 1) {
-				instructions[instructions.length - 1].remove();
-			}
-			if (instructions.length === 1) {
-				this.moreThanOneInstruction = false;
+		removeIngredientAt(index) {
+			if (this.recipe.ingredients.length > 1) {
+				this.recipe.ingredients.splice(index, 1);
 			}
 		},
-		editFirebase() {
-			if (!this.$store.state.admin) {
-				return;
-			}
+    addInstruction() {
+      this.recipe.instructions.push({ checked: false, id: this.recipe.instructions.length, text: "" });
+    },
+    removeInstruction() {
+      if (this.recipe.instructions.length > 1) {
+        this.recipe.instructions.pop();
+      }
+    },
+    editFirebase() {
+      if (!this.$store.state.admin) {
+        return;
+      }
 
-			const db = firebase.database();
-			const database = firebase.database().ref("recipes");
-			const dbKeys = [];
+      const db = firebase.database();
+      const database = firebase.database().ref("recipes");
+      const dbKeys = [];
 
-			database.on("value", (snapshot) => {
-				for (const childSnapshot of snapshot) {
-					dbKeys.push(childSnapshot.key);
-				}
-			});
+      database.on("value", (snapshot) => {
+        for (const childSnapshot of snapshot) {
+          dbKeys.push(childSnapshot.key);
+        }
+      });
 
-			const recipeFirebase = db.ref(`recipes/${dbKeys[this.$route.params.id]}`);
-
-			const title = document.getElementById("title").value;
-			const drink = document.getElementById("drink").checked;
-			const description = document.getElementById("description").value;
-			const ingredients = [];
-			const ingredientsGroups =
-				document.getElementsByClassName("editIngredients");
-			for (let i = 0; i < ingredientsGroups.length; i++) {
-				const ingredient = {
-					amount: ingredientsGroups[i].querySelectorAll("input")[0].value,
-					measurment: ingredientsGroups[i].querySelectorAll("input")[1].value,
-					name: ingredientsGroups[i].querySelectorAll("input")[2].value,
-					section: ingredientsGroups[i].querySelectorAll("input")[3].value,
-				};
-				ingredients.push(ingredient);
-			}
-			const instructions = [];
-			const instructionsGroups =
-				document.getElementsByClassName("editInstructions");
-			for (let i = 0; i < instructionsGroups.length; i++) {
-				const instruction = {
-					checked: instructionsGroups[i].querySelectorAll("input")[0].value,
-					id: instructionsGroups[i].querySelectorAll("input")[1].value,
-					text: instructionsGroups[i].querySelectorAll("input")[2].value,
-				};
-				if (instruction.checked === "true") {
-					instruction.checked = true;
-				}
-				if (instruction.checked === "false") {
-					instruction.checked = false;
-				}
-				instructions.push(instruction);
-			}
-			const servings = document.getElementById("servings").value;
-			const link = document.getElementById("link").value;
-			const imgLink = document.getElementById("imgLink").value;
-			let recipeId = this.$route.params.id;
-			recipeId = Number.parseInt(recipeId);
-			const recipeData = {
-				title: title,
-				drink: drink,
-				description: description,
-				ingredients: ingredients,
-				instructions: instructions,
-				servings: servings,
-				link: link,
-				imgLink: imgLink,
-				recipeId: recipeId,
-			};
-			recipeFirebase.update(recipeData);
-			alert("Recipe updated!");
-			this.$router.push(`/recipe/${this.$route.params.id}`);
-		},
-		putRecipeDetailsInInputs() {
-			document.getElementById("title").value = this.currentRecipe.title;
-			document.getElementById("description").value =
-				this.currentRecipe.description;
-			document.getElementById("imgLink").value = this.currentRecipe.imgLink;
-
-			if (this.currentRecipe.drink) {
-				document.getElementById("drink").checked = true;
-			}
-
-			const ingredients = this.currentRecipe.ingredients;
-			if (ingredients.length > 1) {
-				this.moreThanOneIngredient = true;
-			}
-			for (let i = 0; i < ingredients.length; i++) {
-				const ingredient = ingredients[i];
-				const amount = ingredient.amount;
-				const measurment = ingredient.measurment;
-				const name = ingredient.name;
-				const section = ingredient.section;
-				const newIngredient = document.createElement("div");
-				newIngredient.classList.add("editIngredients");
-				newIngredient.innerHTML = `
-					<input type="text" name="amount" class="amount" value="${amount}" />
-					<input type="text" name="measurment" class="measurment" value="${measurment}" />
-					<AutoCompletingIngredientInput placeholder="Tomatoes" value="${name}" />
-          <input type="text" name="section" class="section" value="${section}" />
-				`;
-				document.getElementById("ingredientsList").appendChild(newIngredient);
-			}
-
-			const instructions = this.currentRecipe.instructions;
-			if (instructions.length > 1) {
-				this.moreThanOneInstruction = true;
-			}
-			for (let i = 0; i < instructions.length; i++) {
-				const instruction = instructions[i];
-				const checked = instruction.checked;
-				const id = instruction.id;
-				const text = instruction.text;
-				const newInstruction = document.createElement("div");
-				newInstruction.classList.add("editInstructions");
-				newInstruction.innerHTML = `
-					<input type="text" name="checked" class="checked" value="${checked}">
-					<input type="number" name="id" class="id" value="${id}">
-					<input type="text" name="text" class="text" value="${text}">
-				`;
-				document.getElementById("instructionsList").appendChild(newInstruction);
-			}
-
-			document.getElementById("servings").value = this.currentRecipe.servings;
-			document.getElementById("link").value = this.currentRecipe.link;
-		},
-	},
-	mounted() {
-		this.putRecipeDetailsInInputs();
-	},
-	computed: {
-		currentRecipe() {
-			return this.$store.state.recipesList[this.$route.params.id];
-		},
-	},
+      const recipeFirebase = db.ref(`recipes/${dbKeys[this.$route.params.id]}`);
+      recipeFirebase.update(this.recipe);
+      alert("Recipe updated!");
+      this.$router.push(`/recipe/${this.$route.params.id}`);
+    },
+    putRecipeDetailsInInputs() {
+      const currentRecipe = this.currentRecipe;
+      this.recipe = JSON.parse(JSON.stringify(currentRecipe)); // Deep copy to avoid mutating the store
+    },
+  },
+  mounted() {
+    this.putRecipeDetailsInInputs();
+  },
+  computed: {
+    currentRecipe() {
+      return this.$store.state.recipesList[this.$route.params.id];
+    },
+  },
 };
 </script>
 
@@ -272,17 +188,11 @@ body {
 #container {
   margin: 20px auto;
   padding: 10px;
-  max-width: 500px;
-}
+  max-width: 95vw;
 
-#container input {
-  display: block;
-  width: 100%;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-  border: 1px solid rgba(0, 0, 0, 0.3);
-  border-radius: 3px;
-  padding: 4px;
-  margin-bottom: 10px;
+	@media (min-width: 600px) {
+		max-width: 80vw;
+	}
 }
 
 #drink {
@@ -296,28 +206,6 @@ body {
   padding: 10px;
 }
 
-#ingredientsButtons,
-#instructionsButtons {
-  display: flex;
-  justify-content: space-between;
-  background-color: #cbf3ff;
-  padding: 10px;
-  padding-top: 0px;
-  margin-bottom: 10px;
-}
-
-#ingredientsButtons button,
-#instructionsButtons button {
-  display: block;
-  background-color: #4a8ee7;
-  color: white;
-  padding: 5px;
-  border: none;
-  border-radius: 10px;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-  cursor: pointer;
-}
-
 .editInstructions,
 .editIngredients {
   display: flex;
@@ -329,22 +217,6 @@ body {
   .section,
   .measurment {
     width: 25% !important;
-  }
-}
-
-#updateRecipeBtn {
-  display: block;
-  margin: 0 auto;
-  background-color: #4a8ee7;
-  color: white;
-  padding: 5px;
-  border: none;
-  border-radius: 10px;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
-  cursor: pointer;
-
-  &:hover {
-    background-color: #3a7dd6;
   }
 }
 
