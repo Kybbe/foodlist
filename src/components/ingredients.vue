b638872827d586d1f4a0ed925b8cd8ae5398894f
Jacob Klaren <jacob.klaren@outlook.com>
2025-08-15 10:58:34 +0200

    fix ingredients miss with spread

--- src/components/ingredients.vue
+++ src/components/ingredients.vue
@@ -145,6 +145,15 @@ export default {
 				return 0;
 			});
 		},
+		ingredientsSortedAlphabetically() {
+			return [...this.ingredients].sort((a, b) => {
+				const nameA = a.name.toLowerCase();
+				const nameB = b.name.toLowerCase();
+				if (nameA < nameB) return -1;
+				if (nameA > nameB) return 1;
+				return 0;
+			});
+		},
 	},
 	methods: {
 		roundToTwoDecimals(num) {
